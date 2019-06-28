c79333a6215d40e2318379afcbf1033abb402881
chaofengwu <wuchaofeng47@gmail.com>
2019-06-28 12:59:09 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup, find_packages
+import pip
+
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
 
 with open('requirements.txt', 'r') as f:
   install_requirements = f.readlines()
+
+for package in install_requirements:
+	install(package)
 
 setup(
   name = "paropt",
@@ -9,6 +21,6 @@ setup(
   author = "Ted Summer",
   author_email = "ted.summer2@gmail.com",
   description = ("Automates optimization of tools"),
-  install_requires=install_requirements,
+  # install_requires=install_requirements,
   packages=find_packages()
 )
