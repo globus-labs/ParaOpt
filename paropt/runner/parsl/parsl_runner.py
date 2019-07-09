b220a30554ec2510170e54691af0a7c2050d52c6
chaofengwu <wuchaofeng47@gmail.com>
2019-07-09 00:57:14 -0500

    Update parsl_runner.py

--- paropt/runner/parsl/parsl_runner.py
+++ paropt/runner/parsl/parsl_runner.py
@@ -92,13 +92,13 @@ class ParslRunner:
             parsl.set_stream_logger()
         self._dfk = parsl.load(self.parsl_config)
 
-        logger.info(f'setup additional first task (sleep 1) to avoid problem')
+        logger.info(f'setup additional first task (sleep 180) to avoid problem')
         try:
             setup_script_content = None
             finish_script_content = None
 
             runConfig = paropt.runner.RunConfig(
-                command_script_content='sleep 1',
+                command_script_content='sleep 180',
                 experiment_dict=self.experiment.asdict(),
                 setup_script_content=setup_script_content,
                 finish_script_content=finish_script_content,
@@ -106,7 +106,7 @@ class ParslRunner:
             result = self.parsl_app(runConfig).result()
         except:
             logger.exception(f'error in first task')
-            
+
         logger.info(f'Starting ParslRunner with config\n{self}')
 
         flag = True
