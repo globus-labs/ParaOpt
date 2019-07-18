f005801dcacaf7a09c67f58653f24902a37da62c
chaofengwu <wuchaofeng47@gmail.com>
2019-07-18 13:59:53 -0500

    Update config.py

--- paropt/runner/parsl/config.py
+++ paropt/runner/parsl/config.py
@@ -42,7 +42,6 @@ def parslConfigFromCompute(compute):
             interchange_port_range=(54051, 54100),
             cores_per_worker=1,
             max_workers=1,
-            worker_logdir_root='runinfo',
             provider=AWSProvider(
               image_id=compute.ami,
               instance_type=compute.instance_model,
