606c98db0a114813da0b4a02f3bef7b29db4452d
chaofengwu <wuchaofeng47@gmail.com>
2019-07-24 13:53:02 -0500

    add searchMatrix

--- paropt/runner/parsl/__init__.py
+++ paropt/runner/parsl/__init__.py
@@ -1,9 +1,11 @@
 from .parsl_runner import ParslRunner
 from .config import parslConfigFromCompute as local_config
 from .lib import timeCommand as timeCmd
+from .lib import searchMatrix as searchMatrix
 
 __all__ = [
   'ParslRunner',
   'local_config',
-  'timeCmd'
+  'timeCmd',
+  'searchMatrix'
 ]
