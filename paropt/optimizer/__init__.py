ce0ecdbac729036cf8f3260c053f5c6028783777
chaofengwu <wuchaofeng47@gmail.com>
2019-08-27 21:12:20 -0500

    Update __init__.py

--- paropt/optimizer/__init__.py
+++ paropt/optimizer/__init__.py
@@ -2,12 +2,10 @@ from .bayesian_optimizer import BayesianOptimizer
 from .grid_search import GridSearch
 from .coordinate_search import CoordinateSearch
 from .random_search import RandomSearch
-from .dfs_search import DFSSearch
 
 __all__ = [
   "BayesianOptimizer",
   "GridSearch",
   "CoordinateSearch",
   "RandomSearch",
-  "DFSSearch"
 ]
