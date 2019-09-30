181155bfbb41c80caae695f9c1a0ed08161a9d5f
chaofengwu <wuchaofeng47@gmail.com>
2019-09-30 13:40:56 -0400

    Update lib.py

--- paropt/runner/parsl/lib.py
+++ paropt/runner/parsl/lib.py
@@ -223,14 +223,16 @@ def variantCallerAccu(runConfig, **kwargs):
             # caller time here in sec
             str_res = outs.decode('utf-8')
             res = str_res.strip().split()
-            obj_parameters = {'running_time': total_time, 'precision': float(res[-2]), 'recall': float(res[-1]), 'caller_time': float(res[-3])/1000}
+            # obj_parameters = {'running_time': total_time, 'precision': float(res[-2]), 'recall': float(res[-1]), 'caller_time': float(res[-3])/1000}
             
             # the output of utility, which is used by optimizer
             # obj_output = objective(obj_parameters['caller_time'], obj_parameters['precision'])
 
-            obj_output = f1_obj(obj_parameters['precision'], obj_parameters['recall'])
-            ret_dic['obj_parameters'] = obj_parameters
-            ret_dic['obj_output'] = obj_output
+            # obj_output = f1_obj(obj_parameters['precision'], obj_parameters['recall'])
+            # ret_dic['obj_parameters'] = obj_parameters
+            # ret_dic['obj_output'] = obj_output
+            ret_dic['obj_output'] = total_time
+            ret_dic['obj_parameters'] = {'obj_output': ret_dic['obj_output']}
             
             # timeout_output = objective(timeout, 0)
             return ret_dic
