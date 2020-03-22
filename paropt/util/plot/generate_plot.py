3a2dd273c3a2b07fc706c7b08d3d736d3c7722ef
chaofengwu <wuchaofeng47@gmail.com>
2020-03-22 13:35:26 +0800

    Update generate_plot.py

--- paropt/util/plot/generate_plot.py
+++ paropt/util/plot/generate_plot.py
@@ -12,17 +12,17 @@ def GridSearch_plot_1D(data, plot_info):
     x_vals = sorted(list(set(data[data['param_names'][0]])))
     fig, axes = plt.subplots(len(data['obj_names']), 1, figsize=(12, 8))
     fig.suptitle(f'Experiment {plot_info["experiment_id"]}')
-    for i, obj_name in enumerate(data['obj_names']):
+    for idx, obj_name in enumerate(data['obj_names']):
         y_val_by_x = {x_val: [] for x_val in x_vals}
-        for idx, x_val in enumerate(data[data['param_names'][0]]):
-            y_val_by_x[x_val].append(data[obj_name][idx])
+        for i, x_val in enumerate(data[data['param_names'][0]]):
+            y_val_by_x[x_val].append(data[obj_name][i])
 
         y_vals = [np.mean(y_val_by_x[x_val]) for x_val in x_vals]
         y_errs = [np.std(y_val_by_x[x_val]) for x_val in x_vals]
 
-        axes[i].errorbar(x_vals, y_vals, y_errs)
-        axes[i].set_xlabel(data['param_names'][0])
-        axes[i].set_ylabel(obj_name)
+        axes[idx].errorbar(x_vals, y_vals, y_errs)
+        axes[idx].set_xlabel(data['param_names'][0])
+        axes[idx].set_ylabel(obj_name)
     # plt.legend()
     plot_name = os.path.join(plot_info['plot_dir'], f'{plot_info["experiment_id"]}.png')
     
@@ -35,16 +35,45 @@ def GridSearch_plot_2D(data, plot_info):
     x_vals = sorted(list(set(data[data['param_names'][0]])))
     y_vals = sorted(list(set(data[data['param_names'][1]])))
 
-    x_val_dic = {val: i for i, val in enumerate(x_val)}
-    y_val_dic = {val: i for i, val in enumerate(y_val)}
+    x_val_dic = {val: i for i, val in enumerate(x_vals)}
+    y_val_dic = {val: i for i, val in enumerate(y_vals)}
 
     X_VAL, Y_VAL = np.meshgrid(x_vals, y_vals)
 
-    Z = np.zeros(X_VAL.shape)
-
+    fig, axes = plt.subplots(1, len(data['obj_names']), figsize=(12, 6))
+    fig.suptitle(f'Experiment {plot_info["experiment_id"]}')
     for idx, obj_name in enumerate(data['obj_names']):
-        pass
-    pass
+        Z = []
+        for i in range(len(X_VAL)):
+            Z.append([])
+            for j in range(len(X_VAL[0])):
+                Z[i].append([])
+
+        for i in range(len(data[data['param_names'][0]])):
+            Z[y_val_dic[data[data['param_names'][1]][i]]][x_val_dic[data[data['param_names'][0]][i]]].append(data[obj_name][i])
+
+        Z = np.array(Z)
+        # print(Z.shape)
+        Z = np.mean(Z, axis=2)
+        ZT = np.flipud(Z)
+
+        im = axes[idx].imshow(ZT)
+
+        for i in range(len(ZT)):
+            for j in range(len(ZT[0])):
+                text = axes[idx].text(j, i, round(ZT[i, j], 2), ha="center", va="center", color="w")
+
+        axes[idx].set_title(f'{obj_name}')
+        axes[idx].set_xlabel(data['param_names'][0])
+        axes[idx].set_ylabel(data['param_names'][1])
+        axes[idx].set_xticks(range(len(x_vals)))
+        axes[idx].set_yticks(range(len(y_vals)))
+        axes[idx].set_xticklabels(x_vals)
+        axes[idx].set_yticklabels(reversed(y_vals))
+
+    plot_name = os.path.join(plot_info['plot_dir'], f'{plot_info["experiment_id"]}.png')
+    plt.savefig(plot_name)
+    return {'success': True, 'error': plot_name}
 
 
 def GridSearch_plot(raw_data, plot_info):
@@ -52,7 +81,7 @@ def GridSearch_plot(raw_data, plot_info):
     parameter_names = [i['parameter_name'] for i in raw_data[0]['parameter_configs']]
     objective_names = list(raw_data[0]['obj_parameters'].keys())
     objective_names.append('obj_outcome')
-    print(objective_names)
+    # print(objective_names)
     if len(parameter_names) > 2:
         ret['error'] = 'more than 2 parameters'
         return ret
@@ -80,6 +109,3 @@ def GridSearch_plot(raw_data, plot_info):
         ret = GridSearch_plot_2D(data, plot_info)
 
     return ret
-    pass
-
-
